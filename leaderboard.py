f98db67d186f6bc11e3854f12e6bf1d0eb002d7a
Amen-Haj-Brahim <amanallahhajbrahim2003@gmail.com>
2024-01-01 15:42:32 +0100

    added leaderboard code

---
+++ leaderboard.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+data=pd.read_csv("leaderboard.csv")
+print(data["name"])
+plt.title("leaderboard")
+plt.xlabel("person")
+plt.ylabel("score")
+plt.bar(data["name"],data["record"])
+#plt.plot()
+plt.show()
